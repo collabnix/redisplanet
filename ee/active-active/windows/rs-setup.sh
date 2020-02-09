70738eeb858e0cec009b4039e137a8bd9113dcd1
Collabnix <34368930+collabnix@users.noreply.github.com>
2020-02-09 19:30:09 +0530

    Create rs-setup.sh

---
+++ ee/active-active/windows/rs-setup.sh
@@ -0,0 +1,3 @@
+docker run -d --cap-add sys_resource --network redisnet1 --name redis-node-01 -h redis-node-01  -p 8443:8443 -p 9443:9443 -p 12000:12000 --ip 172.29.0.2 redislabs/redis
+docker run -d --cap-add sys_resource --network redisnet2 --name  redis-node-02 -h redis-node-02 -p 8444:8443 -p 9444:9443 -p 12001:12000 --ip 172.30.0.2 redislabs/redis
+docker run -d --cap-add sys_resource --network redisnet3 --name  redis-node-03 -h redis-node-03 -p 8445:8443 -p 9445:9443 -p 12002:12000 --ip 172.31.0.2 redislabs/redis
