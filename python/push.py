4fcc07539bbc8f480e3fec87eb7ca27d831e30d6
Collabnix <34368930+collabnix@users.noreply.github.com>
2020-03-19 13:49:55 +0530

    Update push.py

--- python/push.py
+++ python/push.py
@@ -1,6 +1,6 @@
 import redis
 # Create connection object
-r = redis.Redis(host='ajeet-1shard-noreplication.8mys2u.ng.0001.use2.cache.amazonaws.com', port=6379)
+r = redis.Redis(host='<Redis DB Endpoint>', port=6379)
 # set a value for the foo object
 r.set('foo', 'bar')
 # retrieve and print the value for the foo object
