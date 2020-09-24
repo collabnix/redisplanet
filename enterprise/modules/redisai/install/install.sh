5e34771695e7023ecd14d448cf0b0de0319c7caf
Collabnix <34368930+collabnix@users.noreply.github.com>
2020-09-24 17:10:30 +0530

    Update and rename install_rediai.sh to install.sh

--- enterprise/modules/redisai/install/install.sh
+++ enterprise/modules/redisai/install/install.sh
@@ -15,17 +15,6 @@ mkdir deps/linux-x64-cpu/libtorch
 
 cd deps/linux-x64-cpu/libtorch
 
-# https://forums.developer.nvidia.com/t/pytorch-for-jetson-nano-version-1-5-0-now-available/72048
-# wget https://nvidia.box.com/shared/static/phqe92v26cbhqjohwtvxorrwnmrnfx1o.whl -O torch-1.3.0-cp36-cp36m-linux_aarch64.whl
-
-# unzip torch-1.3.0-cp36-cp36m-linux_aarch64.whl
-# mv torch/* .
-
-#wget https://nvidia.box.com/shared/static/ncgzus5o23uck9i5oth2n8n06k340l6k.whl -O torch-1.4.0-cp36-cp36m-linux_aarch64.whl
-# sudo apt install -y libopenblas-base
-
-# unzip torch-1.4.0-cp36-cp36m-linux_aarch64.whl
-# mv torch/* .
 
 wget https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl -O torch-1.5.0-cp36-cp36m-linux_aarch64.whl
 sudo apt install -y libopenblas-base
